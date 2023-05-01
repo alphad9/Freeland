ba362da7d486cc519df0fa90729217b3525fb432
alphad9 <diabyboubacar11@gmail.com>
2023-05-01 13:20:23 +0000

    avancement dans le jeu
    le personnage est cense bouger mais; probleme

--- Freeland/game.py
+++ Freeland/game.py
@@ -17,28 +17,50 @@ class Game:
         tmx_data = pytmx.util_pygame.load_pygame('carte.tmx')
         map_data = pyscroll.data.TiledMapData(tmx_data)
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom = 1
+        map_layer.zoom = 2.5
 
         #generer un joueur
         player_position=tmx_data.get_object_by_name("player")
         self.player = Player(player_position.x,player_position.y)
 
         #dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
         self.group.add(self.player)
 
 
+    '''gerer les deplacement du joueur'''
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+            self.player.change_animation('up')
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+            self.player.change_animation('down')
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+            self.player.change_animation('left')
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+            self.player.change_animation('right')
+
+    print
+
+
     def run(self):
+
+        clock= pygame.time.Clock()
         #boucle du jeu
         running = True
         while running:
+            self.handle_input()
             self.group.update()
             self.group.center(self.player.rect.center)
-
             self.group.draw(self.screen)
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+            clock.tick(60)
         pygame.quit()
--- Freeland/player.py
+++ Freeland/player.py
@@ -11,6 +11,25 @@ class Player(pygame.sprite.Sprite):
         #position
         self.rect= self.image.get_rect()
         self.position = [x, y]
+        self.image ={
+            'down': self.get_image(0,0),
+            'left': self.get_image(0,33.25),
+            'right':self.get_image(0,66.50),
+            'up':self.get_image(0,99.75)
+        }
+        self.speed = 2
+
+    def change_animation(self,name):
+        self.image = self.images[name]
+        self.image.set_colorkey((0,0,0))
+
+
+    def move_right(self): self.position[0]+= self.speed
+    def move_left(self): self.position[0]-=self.speed
+    def move_up(self): self.position[1]-=self.speed
+    def move_down(self):self.position[1] +=self.speed
+
+
 
     def update(self):
         self.rect.topleft = self.position
